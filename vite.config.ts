85da2676b1fdfb92292c622204522cb705a140aa
Rúben Araújo <ruben.araujo@live.com>
2025-08-21 10:17:35 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/lumiere-cinema-selection/",
+  base: "/project_lumiere/",
   server: {
     host: "::",
     port: 8080,
